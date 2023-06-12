88f08040e60c39e57b1946143c6bceedaa666db2
aravindunni18 <aravindunni18@users.noreply.github.com>
2023-06-12 14:50:09 +0530

    AddedHigherSolvationCluster90

--- Cluster90HigherSolvation/SurfAndContPlot.m
+++ Cluster90HigherSolvation/SurfAndContPlot.m
@@ -1,8 +1,7 @@
-% close all
-% clear all
-figure
-M=linspace(54,90,37);
-S=linspace(2,42,41);
+close all
+clear all
+M=linspace(42,87,46);
+S=linspace(22,68,47);
 G=load('free_energy.dat');
 surf(M,S,G');xlabel('M');ylabel('S');
 figure
